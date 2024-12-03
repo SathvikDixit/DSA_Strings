e08bd9cbc3008477269e4baddf49b784af6135c3
SathvikDixit <sathvikdixit21@gmail.com>
2024-12-03 20:54:32 +0530

    dsfasfqwerqwER

--- Search2dMatrix.java
+++ Search2dMatrix.java
@@ -49,7 +49,7 @@ public class Search2dMatrix {
             {1, 3, 5, 7},
             {10, 11, 16, 20},
             {23, 30, 34, 60}
-        };
+        };     
 
         int target = 3;
         boolean result = solution.searchMatrix(matrix, target);
